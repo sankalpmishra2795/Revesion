c83015a47cd1783946c4462f43fc214f6c6235e0
Sankapl Mishra <62585077+sankalpmishra2795@users.noreply.github.com>
2020-11-03 22:04:34 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,27 @@
-let val;
+const movie = new Movie();
 
-val = document;
-val = document.all;
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+const ui = new UI();
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+const searchuser = document.querySelector(".searchinput");
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
-
-val = document.images;
-
-val = document.scripts;
-val = document.scripts[2].getAttribute("src");
-
-let scripts = document.scripts;
-
-let scriptsArr = Array.from(scripts);
-
-scriptsArr.forEach(function (script) {
-  console.log(script.getAttribute("src"));
+// addEventListener
+searchuser.addEventListener("click", (e) => {
+  console.log("-------");
+  // Get text
+  const userText = document.querySelector(".userinput").value;
+  console.log(userText);
+  if (userText !== "") {
+    movie.getUser(userText).then((data) => {
+      if (data.movie.message === "Not Found") {
+        // show alert
+        ui.showAlert("User Not Found", "alert alert-danger");
+      } else {
+        // show profile
+        ui.showProfile(data.movie);
+      }
+    });
+  } else {
+    // clear fields
+    ui.clearProfile();
+  }
 });
-
-console.log(val);
