bf4fa0b141fc1bf4aa9472b551dc671c6d81f22e
Sankapl Mishra <62585077+sankalpmishra2795@users.noreply.github.com>
2020-11-09 20:56:44 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,65 @@
-const movie = new Movie();
+let min = 1,
+  max = 10,
+  winningNum = getRandomNum(min, max),
+  guessesLeft = 3;
 
-const ui = new UI();
+//   UI element
+const game = document.querySelector("#game"),
+  minNum = document.querySelector(".min-num"),
+  maxNum = document.querySelector(".max-num"),
+  guessBtn = document.querySelector("#guess-btn"),
+  guessInput = document.querySelector("#guess-input"),
+  message = document.querySelector(".message");
 
-const searchuser = document.querySelector(".searchinput");
+//   asine UI moin max
+minNum.textContent = min;
+maxNum.textContent = max;
 
-// addEventListener
-searchuser.addEventListener("click", (e) => {
-  console.log("-------");
-  // Get text
-  const userText = document.querySelector(".userinput").value;
-  console.log(userText);
-  if (userText !== "") {
-    movie.getUser(userText).then((data) => {
-      if (data.movie.message === "Not Found") {
-        // show alert
-        ui.showAlert("User Not Found", "alert alert-danger");
-      } else {
-        // show profile
-        ui.showProfile(data.movie);
-      }
-    });
+guessBtn.addEventListener("click", function () {
+  let guess = parseInt(guessInput.value);
+
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter the number between ${min} and ${max}`, "red");
+  }
+  //   win
+  if (guess === winningNum) {
+    guessInput.disable = true;
+    guessInput.style.borderColor = "green";
+    setMessage(`${winningNum} is correct. YOU WIN!`, "green");
+  }
+  //   lost
+  else {
+    guessesLeft -= 1;
+  }
+
+  if (guessesLeft === 0) {
+    guessInput.disable = true;
+    guessInput.style.borderColor = "red";
+    setMessageobject(` The correct number is ${winningNum}.YOU LOST!`, "green");
   } else {
-    // clear fields
-    ui.clearProfile();
+    guessInput.style.borderColor = "red";
+    guessInput.value = "";
+    setMessage(`${guess} is not correct,${guessesLeft} guesses left`, "red");
+  }
+
+  //   play again
+  guessBtn.value = "Play again";
+  guessBtn.className += "play-again";
+});
+
+// add eventlistnere
+game.addEventListener("mousedown", function (e) {
+  if (e.target.className === "play-again") {
+    window.location.reload();
   }
 });
+
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// set message
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
